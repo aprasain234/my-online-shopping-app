8b9a71c099a59877daee7f5cc6421a14ae787a81
Ashish Prasain <aprasain345@gmail.com>
2026-05-19 21:01:10 -0500

    second commit

---
+++ src/main/java/com/ashishprasain/myonlineshoppingapp/MyOnlineShoppingAppApplication.java
@@ -0,0 +1,13 @@
+package com.ashishprasain.myonlineshoppingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOnlineShoppingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyOnlineShoppingAppApplication.class, args);
+    }
+
+}
